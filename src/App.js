64909d9f98924d6efbcac5c9433dff0c5f6f1979
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-01 13:47:14 +0530

    api link added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      rockets : []
+    };
+  }
+
+  componentDidMount(){
+    fetch("https://api.spacexdata.com/v4/rockets")
+    .then((response) =>(response.json()))
+    .then(rockets => console.log(rockets))
+  }
+
+  render() {
+    // function App(){
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
