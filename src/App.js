16c05bac3bd941025899169418cb7f16bdede4db
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-02 18:02:47 +0530

    images with proper padding adjusted

--- src/App.js
+++ src/App.js
@@ -21,11 +21,13 @@ class App extends Component{
     // function App(){
     return (
       <div className="container">
+        <div className="row">
+        
           {this.state.rockets.map((rocket) => (
 
             // <h1 key={rocket.id}>{rocket.name}</h1>
-            <div className="card" key={rocket.id}>
-  <img src="..." className="card-img-top" alt="..."/>
+            <div className="card col-sm-6 " key={rocket.id}>
+  <img src="{rocket.flicker_image}" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{rocket.name}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,6 +35,7 @@ class App extends Component{
   </div>
 </div>
           ) )}
+          </div>
 
       </div>
     );
