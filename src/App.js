fe4775a2a7b07323d4f2f44004a1e266acd0ede7
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-02 18:15:00 +0530

    background image sett

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ class App extends Component{
           {this.state.rockets.map((rocket) => (
 
             // <h1 key={rocket.id}>{rocket.name}</h1>
-            <div className="card col-sm-6 " key={rocket.id}>
-  <img src="{rocket.flicker_image}" className="card-img-top" alt="..."/>
+            <div className="card col-sm-6" key={rocket.id}>
+  <img src={rocket.flickr_images[1]} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{rocket.name}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <a href="#" className="btn btn-primary">View More</a>
   </div>
 </div>
           ) )}
