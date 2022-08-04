fc2606dbbf661c6790cef7bbd551e9fd29915586
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-04 20:52:49 +0530

    carousel added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -22,8 +22,60 @@ export const Modal = ({ rocket }) => (
           </button>
         </div>
         <div className="modal-body">
-
-            
+          <div
+            id={`carousel${rocket.id}Controls`}
+            class="carousel slide"
+            data-ride="carousel"
+          >
+            <ol class="carousel-indicators">
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="0"
+                class="active"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="1"
+              ></li>
+              <li
+                data-target="#carouselExampleIndicators"
+                data-slide-to="2"
+              ></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src= {rocket.flickr_images[0]} class="d-block w-100" alt="..." />
+              </div>
+              <div class="carousel-item">
+                <img src={rocket.flickr_images[1]}lass="d-block w-100" alt="..." />
+              </div>
+             
+            </div>
+            <a
+              class="carousel-control-prev"
+              href={`#carousel${rocket.id}Controls`}
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a
+              class="carousel-control-next"
+              href={`#carousel${rocket.id}Controls`}
+              role="button"
+              data-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
