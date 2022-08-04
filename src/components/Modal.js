18914280ed27043e478b65fc371bed25a8d47399
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-04 21:01:10 +0530

    new carousel added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -27,29 +27,13 @@ export const Modal = ({ rocket }) => (
             class="carousel slide"
             data-ride="carousel"
           >
-            <ol class="carousel-indicators">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="0"
-                class="active"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="1"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="2"
-              ></li>
-            </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src= {rocket.flickr_images[0]} class="d-block w-100" alt="..." />
+                <img src={rocket.flickr_images[0]} class="d-block w-100" alt="..." />
               </div>
               <div class="carousel-item">
-                <img src={rocket.flickr_images[1]}lass="d-block w-100" alt="..." />
+                <img src={rocket.flickr_images[1]} class="d-block w-100" alt="..." />
               </div>
-             
             </div>
             <a
               class="carousel-control-prev"
